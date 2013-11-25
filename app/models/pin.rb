9e6f6776570ea7ab07107a57c5129c2d3545a7c9
waffles <waffles4dinner@gmail.com>
2013-11-25 14:46:40 +0100

    created pin page. added large style for images

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,4 +1,4 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" } # todo: add "image is missing" URL
+	has_attached_file :image, :styles => { :large => "900x900#", :medium => "300x300#>", :thumb => "100x100#>" } # todo: add "image is missing" URL
 end
