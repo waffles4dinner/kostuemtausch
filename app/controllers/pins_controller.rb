7eab4f5235f5e35ccf52b7f27d3cbf9b2823b00d
waffles <waffles4dinner@gmail.com>
2013-11-25 18:03:20 +0100

    Update show view, add glyphicons, change root path

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -2,6 +2,7 @@ class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
+
   def index
     @pins = Pin.all
   end
@@ -46,7 +47,7 @@ class PinsController < ApplicationController
 
     def correct_user
       @pin = current_user.pins.find_by(id: params[:id])
-      redirect_to pins_path, notice: "Not authorized to edit this pin." if @pin.nil?
+      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
