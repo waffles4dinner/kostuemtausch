8d77f66c23657481c2cf2b8b4f52a123f8ab29a6
alex <dgnz@gmx.net>
2013-11-29 21:30:43 +0100

    Finished Version 0.1

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,6 +1,6 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	has_attached_file :image, :styles => { :medium => "600x600>", :thumb => "100x100>" }
 
 	validates :image, presence: true
 	validates :description, presence: true
